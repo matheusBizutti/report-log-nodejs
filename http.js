0379c35d7071fe272d20413024b7c77668d1680a
Matheus Bizutti <matheus.mbizutti@gmail.com>
2017-11-06 10:41:12 -0200

    padronização do obj em lowercase - Protheus não é case sensitive

--- http.js
+++ http.js
@@ -1,7 +1,7 @@
 function validPort(config){
     let message = 'Portas OK\r\n\r\n';
-    if(config.http.port == config.RESTCONFIG.restPort){
-        message = 'A porta [HTTP]: ' + config.http.port + ' deve ser diferente da porta [RESTCONFIG]: ' + config.RESTCONFIG.restPort + '\r\n';
+    if(config.http.port == config.restconfig.restport){
+        message = 'A porta [HTTP]: ' + config.http.port + ' deve ser diferente da porta [RESTCONFIG]: ' + config.restconfig.restport + '\r\n';
     }
     return message;
 }
